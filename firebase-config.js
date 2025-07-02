ae27ba40dd18d8a457ff7c2296395873ea713c7c
Bartersoto <sotobarter44@gmail.com>
2025-07-02 00:46:24 -0400

    a

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,18 @@
-// Importa las funciones necesarias de Firebase
+// firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Configuración de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyD_9f_ftz-Hqy0GQ3O-_14YBLrEae5qNmU",
   authDomain: "vitrinachiloe-dd4da.firebaseapp.com",
   projectId: "vitrinachiloe-dd4da",
-  storageBucket: "vitrinachiloe-dd4da.appspot.com", // corregido .app a .com
+  storageBucket: "vitrinachiloe-dd4da.appspot.com",
   messagingSenderId: "242783206807",
   appId: "1:242783206807:web:6fe1574ebb77825fc3d8d2"
 };
 
-// Inicializa la app Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta la autenticación y la base de datos
 export const auth = getAuth(app);
 export const db = getFirestore(app);
