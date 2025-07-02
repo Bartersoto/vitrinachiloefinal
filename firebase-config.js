1f538b9c0d23481db8027a2e93887cadfdf06ee5
Bartersoto <sotobarter44@gmail.com>
2025-07-02 00:38:00 -0400

    hola

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,21 @@
-// Import the functions you need from the SDKs you need
+// Importa las funciones necesarias de Firebase
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Configuración de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyD_9f_ftz-Hqy0GQ3O-_14YBLrEae5qNmU",
   authDomain: "vitrinachiloe-dd4da.firebaseapp.com",
   projectId: "vitrinachiloe-dd4da",
-  storageBucket: "vitrinachiloe-dd4da.firebasestorage.app",
+  storageBucket: "vitrinachiloe-dd4da.appspot.com", // corregido .app a .com
   messagingSenderId: "242783206807",
   appId: "1:242783206807:web:6fe1574ebb77825fc3d8d2"
 };
 
-// Initialize Firebase
+// Inicializa la app Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta la autenticación y la base de datos
+export const auth = getAuth(app);
+export const db = getFirestore(app);
