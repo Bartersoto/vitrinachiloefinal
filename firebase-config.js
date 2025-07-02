35491e3260de867d29b12ffb7131bb6c82b6806e
Bartersoto <sotobarter44@gmail.com>
2025-07-02 00:33:13 -0400

    a

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_9f_ftz-Hqy0GQ3O-_14YBLrEae5qNmU",
+  authDomain: "vitrinachiloe-dd4da.firebaseapp.com",
+  projectId: "vitrinachiloe-dd4da",
+  storageBucket: "vitrinachiloe-dd4da.firebasestorage.app",
+  messagingSenderId: "242783206807",
+  appId: "1:242783206807:web:6fe1574ebb77825fc3d8d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
