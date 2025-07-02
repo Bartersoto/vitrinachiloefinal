af325b733f435ecbb0c3263ab128e34e9136331d
Bartersoto <sotobarter44@gmail.com>
2025-07-02 02:17:53 -0400

    z

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
 // firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Configuración de Firebase (API key válida)
 const firebaseConfig = {
-  apiKey: "AIzaSyD_9f_ftz-Hqy0GG3Q--14YBLrEae5qNmU",
+  apiKey: "AIzaSyD_9f_ftz-Hqy0GG3Q--14YBLrEae5qNmU", // <- la que viste en Google Console
   authDomain: "vitrinachiloe-dd4da.firebaseapp.com",
   projectId: "vitrinachiloe-dd4da",
   storageBucket: "vitrinachiloe-dd4da.appspot.com",
@@ -14,7 +12,6 @@ const firebaseConfig = {
   appId: "1:242783206807:web:6fe1574ebb77825fc3d8d2"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
